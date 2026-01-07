97363b46d0093acb99fb554a732372c7b204b3d1
ayushduplicate <ayush.233084103@vcet.edu.in>
2026-01-07 23:50:18 +0530

    day 2 professional POST API with validated request body.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return{"message":"welcome 🚀"}
+class Transaction(BaseModel): #data iherit from basemodel this is class /BaseModel = Parent
+
+
+    amount: float
+    type: str
 
 @app.post("/transactions")
-def create_transaction(amount:float):
-    return{
-        "status":"success",
-        "amount_recived": amount
+def create_transaction(transaction: Transaction): ##Transaction : type{transaction} 
+    return {
+        "status": "success",
+        "amount_received": transaction.amount,
+        "type": transaction.type
     }
