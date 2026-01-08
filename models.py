4d63832a8bf23ee1c721acbd6d883f2815fe47b6
ayushduplicate <ayush.233084103@vcet.edu.in>
2026-01-08 19:17:09 +0530

    models added

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, Float, String
+from database import Base
+
+class TransactionDB(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    type = Column(String, nullable=False)
